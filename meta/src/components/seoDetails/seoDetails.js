55a4b4c60c67e2fced259e04e13a71950c924881
Ik00k0 <polkalopeyi@gmail.com>
2021-05-29 19:45:28 +0100

    Used %PUBLIC_URL%/ before each of the Favicon links and corrected phone number in SEO from string.

--- meta/src/components/seoDetails/seoDetails.js
+++ meta/src/components/seoDetails/seoDetails.js
@@ -2,7 +2,7 @@ const seoDetails = {
     instagram: "",
     email: "enquires@metathemaker.com",
     twitter: "",
-    phoneNumber: "+2348067648809"
+    phoneNumber: 2348067648809
 }
 
 export default seoDetails
