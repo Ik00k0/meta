cc8f65b0798c2f3fcf0af02ee91704e609754207
Ik00k0 <polkalopeyi@gmail.com>
2021-05-29 19:29:54 +0100

    Corrected Favicon pointing to wrong location

--- meta/src/components/seoDetails/seoDetails.js
+++ meta/src/components/seoDetails/seoDetails.js
@@ -2,7 +2,7 @@ const seoDetails = {
     instagram: "",
     email: "enquires@metathemaker.com",
     twitter: "",
-    phoneNumber: ""
+    phoneNumber: "+2348067648809"
 }
 
 export default seoDetails
